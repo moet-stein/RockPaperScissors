ca9f23d6f91c246b08ec71bc1d6a25be2644e82b
moet-stein <0101.mste@gmail.com>
2022-01-31 10:13:16 +0100

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Philipp Steinmüller on 2022/01/31.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
